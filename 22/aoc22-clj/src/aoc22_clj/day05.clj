1abde0c54facef1f873a319b85cb9cb47fbf29b7
anntnzrb <anntnzrb@protonmail.com>
2022-12-13 22:06:11 -0500

    d05: finish

--- 22/aoc22-clj/src/aoc22_clj/day05.clj
+++ 22/aoc22-clj/src/aoc22_clj/day05.clj
@@ -4,7 +4,7 @@
 
 (def input
   (let [day     "05"
-        sample? true]
+        sample? false]
     (util/day-input (str day (when sample? "-sample")))))
 
 (defn strings-to-ints
@@ -12,10 +12,19 @@
   [coll]
   (map read-string coll))
 
-(def stacks ['() '(\N \Z) '(\D \C \M) '(\P)])
+(def stacks ['()
+             '(\Q \F \L \S \R)
+             '(\T \P \G \Q \Z \N)
+             '(\B \Q \M \S)
+             '(\Q \B \C \H \J \Z \G \T)
+             '(\S \F \N \B \M \H \P)
+             '(\G \V \L \S \N \Q \C \P)
+             '(\F \C \W)
+             '(\M \P \V \W \Z \G \H \Q)
+             '(\R \N \C \L \D \Z \G)])
 
 (def instructions (->> (str/split (get (str/split input #"\n\n") 1) #"\n")
-                       (map #(re-seq #"\d" %))
+                       (map #(re-seq #"\d+" %))
                        (map strings-to-ints)))
 
 (defn apply-moves
@@ -30,6 +39,12 @@
        (dec moves))
       (assoc stacks from source to target))))
 
+(defn apply-moves'
+  [stacks [moves from to]]
+  (let [source (drop moves (stacks from))
+        target (flatten (conj (stacks to) (take moves (stacks from))))]
+    (assoc stacks from source to target)))
+
 (defn p1
   "Part 1 solution."
   []
@@ -40,4 +55,8 @@
 
 (defn p2
   "Part 2 solution."
-  [])
+  []
+  (->> (reduce apply-moves' stacks instructions)
+       (filter not-empty)
+       (map first)
+       (apply str)))
