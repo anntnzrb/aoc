c40b126d5f35618e215be02e216f982fa2a24949
anntnzrb <anntnzrb@protonmail.com>
2022-12-19 00:05:18 -0500

    22/clj: main refactor

--- 22/aoc22-clj/src/aoc22_clj/core.clj
+++ 22/aoc22-clj/src/aoc22_clj/core.clj
@@ -8,21 +8,24 @@
    [aoc22-clj.day05 :as d05]
    [aoc22-clj.day06 :as d06]))
 
+(def days
+  {1 {:desc "Calorie Counting"        :parts [d01/p1 d01/p2]}
+   2 {:desc "Rock Paper Scissors"     :parts [d02/p1 d02/p2]}
+   3 {:desc "Rucksack Reorganization" :parts [d03/p1 d03/p2]}
+   4 {:desc "Camp Cleanup"            :parts [d04/p1 d04/p2]}
+   5 {:desc "Supply Stacks"           :parts [d05/p1 d05/p2]}
+   6 {:desc "Tuning Trouble"          :parts [d06/p1 d06/p2]}})
+
 (defn -main
-  ([] (.println *err* "Usage: lein run day.part\nEx (Day 3 Part 1): lein run 3.1"))
+  ([] (.println *err* "Usage: lein run day\nEx (Day 3): lein run 3"))
   ([day]
-   (let [day-parts {"1.1" d01/p1
-                    "1.2" d01/p2
-                    "2.1" d02/p1
-                    "2.2" d02/p2
-                    "3.1" d03/p1
-                    "3.2" d03/p2
-                    "4.1" d04/p1
-                    "4.2" d04/p2
-                    "5.1" d05/p1
-                    "5.2" d05/p2
-                    "6.1" d06/p1
-                    "6.2" d06/p2}]
-     (if-let [part (day-parts day)]
-       (println (part))
-       (.println *err* "Error.")))))
+   (let [day     (Integer/valueOf day)
+         desc    (get-in days [day :desc])
+         parts   (get-in days [day :parts])
+         [p1 p2] parts]
+     (println (format
+               "Selected day: %d
+\"%s\"
+--------------------------------
+Part 1: %s
+Part 2: %s " day desc (p1) (p2))))))
